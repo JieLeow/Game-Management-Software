279aa38470c096dc7dc7244513dd45d63fe1a667
Ethan Soo <ethan.soo@hotmail.com>
2020-11-07 11:01:30 -0800

    Adding backend code for login and registration

---
+++ DuplicateUsernameException.java
@@ -0,0 +1,6 @@
+public class DuplicateUsernameException extends Exception
+{
+	public DuplicateUsernameException(String errorMessage) {
+		super(errorMessage);
+	}
+}
---
+++ UsernameNotFoundException.java
@@ -0,0 +1,6 @@
+public class UsernameNotFoundException extends Exception
+{
+	public UsernameNotFoundException(String errorMessage) {
+		super(errorMessage);
+	}
+}
