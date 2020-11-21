5596d965d448047892c8693047901e5bba78fde2
Li Jie <lijie.leow@sjsu.edu>
2020-11-22 01:56:36 +0800

    add Program Class as data model for TableView columns

---
+++ FrontEnd/src/application/Program.java
@@ -0,0 +1,54 @@
+package application;
+
+import javafx.beans.property.SimpleStringProperty;
+
+public class Program {
+   private final SimpleStringProperty programName = new SimpleStringProperty("");
+   private final SimpleStringProperty programDirectory = new SimpleStringProperty("");
+   private final SimpleStringProperty gameStatus = new SimpleStringProperty("");
+   private final SimpleStringProperty hoursPlayed = new SimpleStringProperty("");
+
+public Program() {
+        this("", "", "", "");
+    }
+ 
+    public Program(String programName, String programDirectory, String hoursPlayed, String gameStatus) {
+        setProgramName(programName);
+        setProgramDirectory(programDirectory);
+        setHoursPlayed(hoursPlayed);
+        setGameStatus(gameStatus);
+    }
+
+    public String getProgramName() {
+        return programName.get();
+    }
+ 
+    public void setProgramName(String fName) {
+        programName.set(fName);
+    }
+        
+    public String getProgramDirectory() {
+        return programDirectory.get();
+    }
+    
+    public void setProgramDirectory(String fName) {
+        programDirectory.set(fName);
+    }
+    
+    public String getHoursPlayed() {
+        return hoursPlayed.get();
+    }
+    
+    public void setHoursPlayed(String fName) {
+        hoursPlayed.set(fName);
+    }
+    
+    public String getGameStatus() {
+        return gameStatus.get();
+    }
+    
+    public void setGameStatus(String fName) {
+        gameStatus.set(fName);
+    }
+    
+}
